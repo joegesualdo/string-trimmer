c6e2ffce16237185124a6f40f933b695380a400e
Joe Gesualdo <joegesualdo@gmail.com>
2016-01-16 16:13:56 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__."/vendor/autoload.php";
+
+// use StringTrimmer\StringTrimmer;
+
+echo StringTrimmer\StringTrimmer::left(" hello");
+?>
---
+++ src/StringTrimmer.php
@@ -0,0 +1,9 @@
+<?php
+namespace StringTrimmer;
+
+class StringTrimmer {
+  public static function left($s){
+    return ltrim($s);
+  }
+}
+?>
